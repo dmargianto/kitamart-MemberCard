7da59ea76692f56716fee050ed064aca5ea3165c
Didik Margianto <didik@ITECs-MacBook-Pro.local>
2017-10-02 08:40:50 +0700

    1. nambah menu swipe di kiri 2. nambah activity barcodescan yg masih kosong

--- app/src/main/java/com/kitamart/didik/membercard/ScanBarcode.java
+++ app/src/main/java/com/kitamart/didik/membercard/ScanBarcode.java
@@ -1,8 +1,18 @@
 package com.kitamart.didik.membercard;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by didik on 10/2/17.
  */
 
-public class ScanBarcode {
+public class ScanBarcode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_barcode);
+    }
 }
--- app/src/main/java/com/kitamart/didik/membercard/Unlock.java
+++ app/src/main/java/com/kitamart/didik/membercard/Unlock.java
@@ -1,8 +1,33 @@
 package com.kitamart.didik.membercard;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.goodiebag.pinview.Pinview;
+
 /**
  * Created by didik on 10/1/17.
  */
 
-public class Unlock {
+public class Unlock extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unlock);
+
+        final Pinview pv = (Pinview) findViewById(R.id.pinView);
+        pv.setPinViewEventListener(new Pinview.PinViewEventListener() {
+            @Override
+            public void onDataEntered(Pinview pinview, boolean b) {
+//                Toast.makeText(Unlock.this, ""+pv.getValue(), Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Unlock.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
 }
